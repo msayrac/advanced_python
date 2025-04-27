95db4ac1fdc3aa828d438d4b8b474a40b0f4da46
Muhammed Sayrac <94198566+msayrac@users.noreply.github.com>
2025-04-27 22:40:55 +0300

    Add files via upload

---
+++ 102Python/102Python_Operators.py
@@ -0,0 +1,65 @@
+#%% operators
+sayi1=12
+sayi2=5
+print(sayi1 + sayi2) # addition
+print(sayi1 - sayi2) # substraction
+print(sayi1 * sayi2) # multiplication
+print(sayi1 / sayi2) # Division
+print(sayi1 % sayi2) # Modulus gives remainder
+print(sayi1 ** sayi2) # Exponnetiation üst alma
+print(sayi1 // sayi2) # Floor division integer division tam kısmı alıyor
+
+#%% Atama operatoru
+# = operatoru
+sayi1=10
+isim ="Ayse"
+print(f"sayi1 : {sayi1}")
+
+sayi1+=5
+print(f"sayi1 : {sayi1}")
+
+sayi1-=5
+print(f"sayi1 : {sayi1}")
+
+sayi1*=5
+print(f"sayi1 : {sayi1}")
+
+sayi1/=5
+print(f"sayi1 : {sayi1} Tipi : {type(sayi1)}")
+
+sayi1%=3
+print(f"sayi1 : {sayi1}")
+
+sayi1=2
+sayi1**=2 # üst alma
+print(sayi1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
